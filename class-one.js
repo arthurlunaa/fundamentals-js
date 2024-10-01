2af79821ecbc15feddb13a660c0472b50fd6579a
arthurlunaa <henriquearthur192@gmail.com>
2024-09-30 21:13:02 -0300

    feat: added continuação do js

--- class-one.js
+++ class-one.js
@@ -18,18 +18,18 @@ console.clear(typeof)
 */
 
 
-let name  = 'Github'
+let name = 'Github'
 const age = 24
-let isApproved =  false
-let date =  null
+let isApproved = false
+let date = null
 let address
 
-const languages =  ['java', 'javascript', 'php']
+const languages = ['java', 'javascript', 'php']
 const numbers = [1, 2, 3]
 
 numbers[3] = 'chocolate'
 
-console.log(numbers,'estou falando desse')
+console.log(numbers, 'estou falando desse')
 
 const user = {
     email: 'teste@teste.com',
@@ -37,8 +37,8 @@ const user = {
     age: 10
 }
 
-const {email, password} = user
-console.log(password , email, 'desestruturei o user')
+const { email, password } = user
+console.log(password, email, 'desestruturei o user')
 
 console.log(user.age)
 console.log(user.email)
@@ -72,7 +72,7 @@ console.log(maiusculo)
 
 // lowerCase
 const minusculo = fullName.toLocaleLowerCase()
-console.log (minusculo)
+console.log(minusculo)
 
 //buscar indice 
 
@@ -80,3 +80,60 @@ console.log(fullName.indexOf('de'), 'indexOf')
 
 // corta string por indice
 console.log(fullName.slice(8, 10))
+
+const remember = 'Mariazinha da Silva '
+
+
+console.log(remember.length,"remember");
+
+let arrayRemember = remember.split(" ");
+
+console.log(remember.split [0], "split");
+
+console.log(remember.slice, (0, 10), "slice")
+
+// metodos de array 
+
+console.log(arrayRemember.length, 'conta indice ')
+
+const nameRule = arrayRemember[0] + ' ' + arrayRemember[arrayRemember.length - 1];
+console.log(nameRule)
+console.log (arrayRemember[1],'segundo item de um array')
+
+arrayRemember[4] = 'inseri um item'
+console.table(arrayRemember)
+
+arrayRemember.push ('adicionei usando o push'
+)
+console.table(arrayRemember)
+
+console.log(arrayRemember[arrayRemember.length -1])
+arrayRemember.pop
+
+console.log(arrayRemember, 'pop')
+
+arrayRemember.shift()
+console.table (arrayRemember, 'shift')
+
+arrayRemember.unshift('novo')
+console.table(arrayRemember, 'unshift')
+
+// objeto
+const product = {
+    name: 'camisa',
+    price: 15.98,
+    inStock: true,
+    brand:'Oakley'
+}
+console.log (product)
+console.log (product.name)
+console.log (product.brand)
+
+const {inStock,price} = product
+console.log(inStock,price)
+
+product.color = ['Azul', 'Vermelho']
+
+
+console.log(product.color[0])
+
