0b18b88cd2024436c9aded28dc74905726417a58
arthurlunaa <henriquearthur192@gmail.com>
2024-10-23 20:58:21 -0300

    create:for js

---
+++ src/js/for.js
@@ -0,0 +1,30 @@
+// loop 
+
+//for(i = 0; i <=5; i ++ ){
+  //  console.log(i)
+
+//}
+ // operador de adição - incremento 
+//let int = 1
+// incremento pos con
+// int = int + 1 (int++)
+//console.log(int++,'pos ')
+
+// incremento pre
+//console.log(++int,'pre ')
+
+//let dec = 10
+// demicremento pos con
+// dec = dec + 1 (dec--)
+//console.log(dec--,'pos ')
+
+// incremento pre
+//console.log(--dec,'pre ')
+
+for (i = 1 ; i<=20; i++ ){
+    if(i % 2 ===0){
+        console.log(i,'é par! ')
+    } else {
+        console.log(i,'impar')
+    }
+}
