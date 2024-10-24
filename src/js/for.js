657d18143070424d018ad9b435c37e6a222eb039
arthurlunaa <henriquearthur192@gmail.com>
2024-10-23 21:27:53 -0300

    create:for ARRAY  js

--- src/js/for.js
+++ src/js/for.js
@@ -1,10 +1,10 @@
 // loop 
 
 //for(i = 0; i <=5; i ++ ){
-  //  console.log(i)
+//  console.log(i)
 
 //}
- // operador de adição - incremento 
+// operador de adição - incremento 
 //let int = 1
 // incremento pos con
 // int = int + 1 (int++)
@@ -21,10 +21,36 @@
 // incremento pre
 //console.log(--dec,'pre ')
 
-for (i = 1 ; i<=20; i++ ){
-    if(i % 2 ===0){
-        console.log(i,'é par! ')
-    } else {
-        console.log(i,'impar')
-    }
-}
+//for (i = 1 ; i<=20; i++ ){
+//  if(i % 2 ===0){
+//     console.log(i,'é par! ')
+//} else {
+//  console.log(i,'impar')
+//}
+//}
+//let object = {
+  //  name: 'maria',
+    //age: 40
+//}
+const products = [{
+    name: ' camiseta',
+    price: 25
+}, {
+    name: ' calça',
+    price: 60
+}, {
+    name: ' chápeu',
+    price: 15
+}]
+// percorre todos os obj do array
+//for (let product of products) {
+  //  console.log(product.name + ' Custa '+ product.price + ' Reais ')
+//}
+ // soma todos 
+ let total = 0
+ for (let product of products) {
+    total += product.price
+    console.log(' A soma total dos produtos é ' + total + ' reais ')
+   
+    
+ }
