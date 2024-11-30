57037128c0446e2ac1446576e47a6f7f1b331ee6
arthurlunaa <henriquearthur192@gmail.com>
2024-11-29 22:33:18 -0300

    feat:function

--- src/js/imc.js
+++ src/js/imc.js
@@ -1,5 +1,18 @@
-function pesoCorporal (imc){
-    const idade = 18
-    const peso = 80
-    if
+function paImc(peso, altura){
+    const Imc = peso / (altura * altura);
+    let classificacao;
+    
+    if (Imc < 18.5) {
+        classificacao = "Abaixo do peso";
+    } else if (Imc < 24.99) {
+        classificacao = "Peso normal";
+    } else if (Imc < 29.99) {
+        classificacao = "Acima do peso";
+    } else {
+        classificacao = "Obesidade";
+    }
+    
+    
+ 
 }
+paImc (104 , 1,85);
