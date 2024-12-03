91ad2edfc48847e71464e10e90e8d3548eb42599
arthurlunaa <henriquearthur192@gmail.com>
2024-12-02 21:47:17 -0300

    feat:consumindo-api

--- src/js/api.js
+++ src/js/api.js
@@ -3,16 +3,16 @@ function getPokemom (pokemom) {
     fetch (`https://pokeapi.co/api/v2/pokemon/${pokemom}`)
     .then (res => res.json())
     .then(data => {
-        const {name, sprites, weigth} = data;
+       const {name, sprites, weight} = data;
         const nome = `Nome: ${name}`;
-        const peso = `Peso: ${weigth}`;
+       const peso = `Peso: ${weight}`;
         const linkfoto = `Link da foto: ${sprites.front_default}`;
         console.log(nome);
         console.log(peso);
-        console.log(linkfoto);
+    console.log(linkfoto);
+   })
 
-    })
-
-    .catch(error => console.error('Error' , error))
+   .catch(error => console.error('Error' , error))
 }
 getPokemom('pikachu')
+
